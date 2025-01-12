3cadd9cdb36b6ff460f97b1193047578294705b7
Faysal-star <kishorepasha347@gmail.com>
2025-01-12 19:40:31 +0600

    usr 404

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,6 +26,9 @@ export async function getProfile(req, res) {
   const { userId } = req.params;
   try {
     const user = await User.findOne({ uid: userId });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
     res.json(user);
   } catch (err) {
     res.status(500).json({ error: 'Server Error' });
